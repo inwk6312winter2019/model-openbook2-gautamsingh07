f43ffd97725170f76a253c1a7d871f763c228f87
Gautam Singh <nonu@TC3FB.WPA.Dal.Ca>
2019-02-06 16:56:24 -0400

    dict updated

--- task1.py
+++ task1.py
@@ -1,7 +1,16 @@
 
-with open('Street_Centerlines.csv', "rt") as file:
+with open("Street_Centerlines.csv") as file:
  
  a = [tuple (line( [STR_NAME],[FULL_NAME],[FROM_STR],[TO_STR] ))for line in file]
 
 print (a)
 
+
+def histogram ():
+ d = {}
+ for row in file:
+  key="MAINTENANCE" 
+  val=number of streets
+  (key,val) = row
+  d[int(key)] = val
+ print(d)
